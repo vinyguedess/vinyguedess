a482a240947a4511b85465a5d6286b064e2a7604
Vinicius Guedes <vinyguedess@DESKTOP1-PARPE.br>
2017-07-19 16:18:44 -0300

    Adding new content and functionalities

--- tsc/src/app.ts
+++ tsc/src/app.ts
@@ -7,16 +7,18 @@ app.get('/', (request, response):void => {
     Promise.all([
         new Service('seo').findByIndex(1),
         new Service('knowledge').find(),
+        new Service('projects').find(),
         new Service('experience').find()
     ])
     .then((resultados:Array<any>):void => {
-        let [seo, conhecimentos, experiencias] = resultados;
+        let [seo, conhecimentos, projetos, experiencias] = resultados;
 
         seo.getFormattedKeyWords = () => seo.keywords.join(', ');
 
         response.render('main', {
             seo: seo,
             conhecimentos: conhecimentos,
+            projetos: projetos,
             experiencias: experiencias
         });
     });
